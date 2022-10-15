bf3ea8261349d89c6aed671af8ded27e486a6cfc
obkin <kapellwork@gmail.com>
2022-10-15 14:41:11 +0300

    add-try/catch

--- script.js
+++ script.js
@@ -15,12 +15,24 @@ const data = [
     }
 ];
 
-data.forEach((blockObj, i) => {
-    const block = document.createElement(blockObj.tag);
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if (!blockObj.id) {
+            throw new SyntaxError(`в данных под номером ${i + 1} отсутствует id.`);
+        }
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch(e) {
+    console.error(e.name);
+    console.log(e.message);
+    console.log(e.stack);
+}
 
-    if (!blockObj.id) throw new Error(`в данных под номером ${i + 1} отсутствует id.`);
-
-    block.setAttribute('id', blockObj.id);
-    document.body.append(block);
-});
-
+// const err = new SyntaxError('gg');
+// console.log(err.name);
+// console.log(err.message);
+// console.log(err.stack);
